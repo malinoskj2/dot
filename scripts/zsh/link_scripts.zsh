95841159c8c5150669581ad894155b4ba656bf2a
Jesse Malinosky <j2@jessemalinosky.com>
2018-10-31 01:41:15 -0400

    Added scripts that sym links all node scripts to env/scripts/bin directory

---
+++ scripts/zsh/link_scripts.zsh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/zsh
+
+# So I am not manually making sym links
+
+local bin_dir="$HOME/env/scripts/bin"
+local scripts=($(find $HOME/env/scripts -name "*.js" | grep -v node_modules | grep -v bin))
+
+local counter=1
+
+for script in $scripts; do
+    echo "counter: $counter " 
+    let "counter = $counter + 1 "
+    local base=${$( basename $script)};
+    local base_min=${$( basename $script ".js" )};
+    print "found node script: $base "
+    ln -sf $script $bin_dir/$base_min
+    print -P "%(0?.successfully linked as $base_min.failed to link $base) \n"
+done
+
+
