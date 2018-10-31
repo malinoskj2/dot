9f3d8a57151c1c794f378c0750ca36fb31f5f4fb
Jesse Malinosky <j2@jessemalinosky.com>
2018-10-31 01:10:15 -0400

    Started adding scripts that will make cloning + install easy.

---
+++ scripts/zsh/link_cfgs.zsh
@@ -0,0 +1,25 @@
+#!/usr/local/bin/zsh
+
+# So I am not manually making sym links
+
+check_exists() {
+if [ -e "$1" ]; then
+  echo "${$( basename $1)} exists"
+else 
+  echo "couldn't find ${$( basename $1)}"
+fi
+}
+
+typeset -A links
+links[$HOME/env/x11/.xinitrc]=~/.xinitrc
+links[$HOME/env/.zsh/.zshrc]=~/.zshrc
+links[$HOME/env/terminal/alacritty.yml]=~/.config/alacritty.yml
+links[$HOME/env/.gitconfig]=~/.gitconfig
+
+for source dest	in ${(kv)links}; do
+    check_exists $source
+    ln -sf $source $dest
+    print -P "%(0?.successfully linked.failed to link) \n"
+done
+
+
