1b9957a62a5c7a17dcc12b4aad519af4601a2b33
Jesse Malinosky <j2@jessemalinosky.com>
2018-10-31 23:24:31 -0400

    Moved scripts related to setting up environment to seperate directory.

--- scripts/zsh/install/link_scripts.zsh
+++ scripts/zsh/install/link_scripts.zsh
@@ -5,11 +5,23 @@
 local bin_dir="$HOME/env/scripts/bin"
 local scripts=($(find $HOME/env/scripts -name "*.js" | grep -v node_modules | grep -v bin))
 
+if [ -d $bin_dir ]; then
+     print "bin directory already exists"
+ else
+     print "bin directory does not exist, creating \n"
+     mkdir $bin_dir
+     print -P "%(0?.successfully created bin directory.failed to create bin dir \n"
+fi      
+
 for script in $scripts; do
     local base=${$( basename $script)};
     local base_min=${$( basename $script ".js" )};
+    
+    echo "source: $script"
+    echo "dest: $bin_dir/$base_min"
+
     print "found node script: $base "
-    ln -sf $script $bin_dir/$base_min
+    ln -sf $script "$bin_dir/$base_min"
     print -P "%(0?.successfully linked as $base_min.failed to link $base) \n"
 done
 
