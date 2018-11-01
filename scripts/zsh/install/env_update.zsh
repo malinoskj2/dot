a5e20719ba3533b9bfe282731c6545651bfd4f84
Jesse Malinosky <j2@jessemalinosky.com>
2018-10-31 23:54:48 -0400

    Added script that pulls updated git repo and makes necessary sym links.

---
+++ scripts/zsh/install/env_update.zsh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/zsh
+
+# Basic env update from git repo
+
+local script_dir=${0:a:h}
+
+git pull origin master
+
+local setup_scripts=( link_cfgs.zsh
+	      	      get_node_depends.zsh
+	              link_scripts.zsh )
+
+for script in $setup_scripts; do
+  $script_dir/$script
+done
