e1fbdfdd6c7f41b920c5eb7f6c6a32f8336a9f1d
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-02 14:07:28 -0400

    Backups are now compressed

--- scripts/sh/zfs_backup_routine.sh
+++ scripts/sh/zfs_backup_routine.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
+HOUR=$(date +"%H")
 DATE=`date +%Y-%m-%d`
 BACKUP_DIR="$1"
 SS_NAME="zroot@nightly_${DATE}"
-FILE_NAME="nightly_${DATE}.zfs"
+FILE_NAME="nightly_${DATE}-H:${HOUR}.zfs.xz"
 
 zfs snapshot -r $SS_NAME &&
-zfs send -R  $SS_NAME > "$BACKUP_DIR/${FILE_NAME}" &&
+zfs send -R  $SS_NAME  | xz -1 --threads=6 > "$BACKUP_DIR/${FILE_NAME}" &&
 zfs list -t snapshot -o name | grep "@nightly" | xargs -n 1 zfs destroy
