77499357950a35b6fd7062a1d919b2eb4c808d38
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-03 02:23:06 -0400

    Refactor Javascript sym-linker into its own function

--- scripts/zsh/install/link_scripts.zsh
+++ scripts/zsh/install/link_scripts.zsh
@@ -3,17 +3,24 @@
 # So I am not manually making sym links
 
 local bin_dir="$HOME/env/scripts/bin"
-local scripts=($(find $HOME/env/scripts -name "*.js" | grep -v node_modules | grep -v bin))
 
-if [ -d $bin_dir ]; then
-     print "bin directory already exists"
- else
-     print "bin directory does not exist, creating \n"
-     mkdir $bin_dir
-     print -P "%(0?.successfully created bin directory.failed to create bin dir \n"
-fi      
+local js_scripts=($(find $HOME/env/scripts -name "*.js" | grep -v node_modules | grep -v bin))
 
-for script in $scripts; do
+local zsh_scripts=($HOME/env/scripts/zsh/*(.))
+
+
+create_bin_dir() {
+  if [ -d $bin_dir ]; then
+    print "bin directory already exists"
+  else
+    print "bin directory does not exist, creating \n"
+    mkdir $bin_dir
+    print -P "%(0?.successfully created bin directory.failed to create bin dir \n"
+  fi      
+}
+
+link_js_scripts() {
+  for script in $js_scripts; do
     local base=${$( basename $script)};
     local base_min=${$( basename $script ".js" )};
     
@@ -23,6 +30,8 @@ for script in $scripts; do
     print "found node script: $base "
     ln -sf $script "$bin_dir/$base_min"
     print -P "%(0?.successfully linked as $base_min.failed to link $base) \n"
-done
+  done
+}
 
-
+create_bin_dir
+link_js_scripts
