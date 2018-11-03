fe605ea89a616aad9abb28d0a39795f0fc5cf13d
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-02 23:28:07 -0400

    Reload at beginning of script.

--- scripts/zsh/mupload.zsh
+++ scripts/zsh/mupload.zsh
@@ -3,7 +3,7 @@
 local file_name=$1
 local remote_dir=/Root/temp
 
-local temp_contents=`megals -e $remote_dir | sed 's/.*Root/\/Root/' | tail -n +2 | wc -l`
+local temp_contents=`megals --reload -e $remote_dir | sed 's/.*Root/\/Root/' | tail -n +2 | wc -l`
 
 if [ "$temp_contents" -gt "25" ]; then
     megarm $remote_dir
@@ -12,6 +12,6 @@ fi
 
 megarm "$remote_dir/$file_name" &> /dev/null
 
-megaput --reload --path /Root/temp ${file_name}
+megaput -path /Root/temp ${file_name}
 
 megals -e $remote_dir | rg $file_name
