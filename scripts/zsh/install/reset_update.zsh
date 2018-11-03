a028cd530942eb81413c1471230e989499fff08e
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-02 23:53:13 -0400

    Added script to do a hard reset then update. It's a slight variation on the script I am typically using to pull from this repo but it will be useful sometimes.

---
+++ scripts/zsh/install/reset_update.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+# Basic env update from git repo
+
+local script_dir=${0:a:h}
+
+git reset --hard origin/master && 
+git pull -ff origin master
+
+local setup_scripts=( link_cfgs.zsh
+	      	      get_node_depends.zsh
+	              link_scripts.zsh )
+
+for script in $setup_scripts; do
+  $script_dir/$script
+done
