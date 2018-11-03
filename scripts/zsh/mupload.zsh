059dfff338f8c67adc25c7b6f3dfcb5ad27f0263
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-02 23:22:37 -0400

    So I dont have to worry about manually interacting with this directory ever. Files are automatically overwritten if a file with the same name is upload. If number of files in the directory exceeds 25 all items are deleted.

--- scripts/zsh/mupload.zsh
+++ scripts/zsh/mupload.zsh
@@ -1,5 +1,17 @@
 #!/usr/bin/env zsh
 
 local file_name=$1
+local remote_dir=/Root/temp
 
-megaput --reload --path /Root/temp $file_name
+local temp_contents=`megals -e $remote_dir | sed 's/.*Root/\/Root/' | tail -n +2 | wc -l`
+
+if [ "$temp_contents" -gt "25" ]; then
+    megarm $remote_dir
+    megamkdir $remote_dir
+fi
+
+megarm "$remote_dir/$file_name" &> /dev/null
+
+megaput --reload --path /Root/temp ${file_name}
+
+megals -e $remote_dir | rg $file_name
