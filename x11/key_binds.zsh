d2f6fd29f89aa474027aa7bdf9b91f0576dfb87e
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-09 14:08:21 -0500

    Bound caps lock to control modifier

--- x11/key_binds.zsh
+++ x11/key_binds.zsh
@@ -2,3 +2,13 @@
 
 xmodmap -e "keycode 51 = BackSpace" # backslash -> BackSpace
 xmodmap -e "keycode 22 = backslash bar" # BackSpace -> backslash bar
+
+xmodmap -e "clear Lock"
+xmodmap -e "clear Control"
+
+xmodmap -e "keycode 66 = Control_L" # Caps_Lock -> Control_L
+
+# Assigns all keys with Keysym Control_L or Control_R to
+# the control modifier. This is both regular control keys + 
+# caps lock.
+xmodmap -e "add Control = Control_L Control_R"
