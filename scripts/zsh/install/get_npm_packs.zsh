c3635432683987726a08d93e4c7fdc37e2e87788
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-16 22:08:31 -0500

    Modified npm global pack installer script to just use global flag.

--- scripts/zsh/install/get_npm_packs.zsh
+++ scripts/zsh/install/get_npm_packs.zsh
@@ -10,23 +10,12 @@ local script_path="${0:A:h}"
 local pkg_cfg="${script_path}/cfg/npm_pkg.cfg"
 local npm_packs=( "${(f)mapfile[$pkg_cfg]}" )
 
-local npm_lib=$HOME/.npm/lib
+
 
 install_npm_packs() {
   for pack in $npm_packs; do
-    npm install --prefix $npm_lib $pack
+    npm install -g $pack
   done
 }
 
-init_node_lib() {
-  if [ -d $npm_lib ]; then
-    print "npm lib dir already exists \n"
-  else
-    print "npm lib dir does not exist \ncreating directory \n"
-    mkdir $npm_lib
-    npm install --prefix $npm_lib
-  fi
-}
-
-init_node_lib
 install_npm_packs
