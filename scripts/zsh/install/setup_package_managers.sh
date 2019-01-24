bff9c5ef234f59d1d9f0fbd6a772a3253bc1eee0
Jesse Malinosky <j2@jessemalinosky.com>
2019-01-24 04:39:03 -0500

    Add script to setup package managers

---
+++ scripts/zsh/install/setup_package_managers.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+function install_native() {
+  if [ $(uname) = 'FreeBSD' ]; then
+    sudo yes | sudo pkg
+  elif [ $(uname) = 'Linux' ]; then
+    sudo apt-get update &&
+    sudo apt update
+  fi
+}
+
+function install_node() {
+  if [ $(uname) = 'FreeBSD' ]; then
+    sudo pkg install -y npm node
+  elif [ $(uname) = 'Linux' ]; then
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo bash - 
+    if $?; then
+      sudo install nodejs
+    fi
+}
+
+function install_rust() {
+  curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --no-modify-path -y &&
+
+rustup target add i686-pc-windows-msvc 
+rustup target add wasm32-unknown-unknown
+rustup target add x86_64-unknown-freebsd
+rustup target add x86_64-unknown-linux-gnu
+rustup component add rustfmt
+}
+
+install_native
+install_node
+install_rust  
