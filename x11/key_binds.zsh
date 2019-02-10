f30d02cb9837dd2146fd4abe2878896dc9491f28
Jesse Malinosky <j2@jessemalinosky.com>
2019-02-10 05:37:03 -0500

    update keybinds

--- x11/key_binds.zsh
+++ x11/key_binds.zsh
@@ -11,60 +11,10 @@ xmodmap -e "clear Control"
 
 # Assigns all keys with Keysym Control_L or Control_R to
 # the control modifier.
+
 xmodmap -e "add Control = Control_L Control_R"
 
-#xmodmap -e "keycode 66 = Control_L" # Caps_Lock -> Control_L
-
-if [[ "$hostname" == "home" ]]; then
 xmodmap -e "keycode 66 = Mode_switch Mode_switch" # Caps_Lock -> Mode_switch
-
-# Disable arrow keys
-
-# Up Bindings
-#xmodmap -e "keycode 98 = Up"     # Up Enable
-xmodmap -e "keycode 98 = "        # Up Disable
-
-# Left Bindings
-#xmodmap -e "keycode 100 = Left"  # Left Enable
-xmodmap -e "keycode 100 = "       # Left Disable
-
-# Down Bindings
-#xmodmap -e "keycode 102 = Down"  # Down Enable
-xmodmap -e "keycode 102 = "       # Down Disable
-
-# Right Bindings
-#xmodmap -e "keycode 104 = Right" # Right Enable
-xmodmap -e "keycode 104 = "       # Right Disable
-
-fi
-
-if [[ "$hostname" == "jesse-cb" ]]; then
-xmodmap -e "keycode 133 = Mode_switch Mode_switch" # Caps_Lock -> Mode_switch
-
-# Disable arrow keys
-
-# Up Bindings
-#xmodmap -e "keycode 111 = Up"     # Up Enable
-xmodmap -e "keycode 111 = "        # Up Disable
-
-# Left Bindings
-#xmodmap -e "keycode 113 = Left"  # Left Enable
-xmodmap -e "keycode 113 = "       # Left Disable
-
-# Down Bindings
-#xmodmap -e "keycode 116 = Down"  # Down Enable
-xmodmap -e "keycode 116 = "       # Down Disable
-
-# Right Bindings
-#xmodmap -e "keycode 114 = Right" # Right Enable
-xmodmap -e "keycode 114 = "       # Right Disable
-
-fi
-
-# Make Mode_switch + ijkl function as arrow keys
-
-#xmodmap -e "keycode 109 = Control_L Control_R"
-
 xmodmap -e "keycode 31 = i I Up" # i binding
 xmodmap -e "keycode 44 = j J Left" # j binding
 xmodmap -e "keycode 45 = k K Down" # k binding
