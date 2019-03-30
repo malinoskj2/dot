3653dff82704399a9b631918488ebb8d0b312520
Jesse Malinosky <jesse@malinoskj2.dev>
2019-03-30 13:43:14 -0400

    update ss script

--- script/sys/screenshot.zsh
+++ script/sys/screenshot.zsh
@@ -7,7 +7,7 @@ mkdir -p "${SS_DIR}" &&
 import "${FILE_PATH}" &&
 echo "screenshot saved @ ${FILE_PATH}" &&
 OUTPUT=$(curl -F file="@${FILE_PATH}" \
-https://imagebin.ca/upload.php | tail -n 1 | sed 's/.*url://')
+https://vgy.me/upload | jq -r .url | sed -e 's/u\///g' -e 's/$/.jpg/')
 echo $OUTPUT 
 echo $OUTPUT | xclip -selection clipboard &&
 notify-send 'Upload Finished!' "${OUTPUT}" -i ${FILE_PATH}
