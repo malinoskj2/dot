057fd7e2f288f4b258bd6eca6d5c1ddcfb8e80a6
Jesse Malinosky <jesse@malinoskj2.dev>
2019-04-05 18:13:38 -0400

    update system config

--- script/virt/up.sh
+++ script/virt/up.sh
@@ -8,7 +8,6 @@ function destroyExisting () {
 }
 
 function checkModLoad () {
-
   kldstat -n vmm > /dev/null 2>&1
   if [ $? -ne 0 ]; then
       errmsg "vmm.ko is not loaded"
@@ -24,16 +23,25 @@ function createInterfaces () {
   ifconfig bridge0 up 
 }
 
-function launchBhyve () {
+function runBhyve () {
 
-  bhyve -AHP -s 0:0,hostbridge -s 1:0,lpc \
-  -s 2:0,virtio-net,tap0 -s 3:0,virtio-blk,./bhyvelinux_storage.img \
-  -s 4:0,ahci-cd,./ubuntu-18.04.2-live-server-amd64.iso -c 4 -m 2048M \
-  -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
-  bhyveguest
+  bhyve -AHP \
+	-s 0:0,hostbridge \
+	-s 1:0,lpc \
+        -s 2:0,virtio-net,tap0 \
+	-s 3:0,virtio-blk,./bhyvelinux_storage.img \
+  	-s 4:0,ahci-cd,./ubuntu-18.04.2-live-server-amd64.iso \
+	-c 1 -m 2048M \
+	-l com1,stdio \
+  	-l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
+  	bhyveguest
+
+  bhyvectl --destroy --vm=bhyveguest
+  ifconfig bridge0 deletem tap0 
+  ifconfig 
 }
 
 checkModLoad
 destroyExisting
 createInterfaces
-launchBhyve
+runBhyve
