aab50cb3b2ddff1ab8c8f44ae5f1eb3e08c99dae
Jesse Malinosky <jesse@malinoskj2.dev>
2019-04-22 18:24:01 -0400

    update install script

--- script/install/install.sh
+++ script/install/install.sh
@@ -4,8 +4,8 @@ SCRIPT_PATH=${0:a:h}
 
 ## ${SCRIPT_PATH}/resource.sh && 
 
-/usr/home/jesse/app/yamlink /usr/home/jesse/env -t dot -c config-map.yaml
-/usr/home/jesse/app/yamlink /usr/home/jesse/resource -c config-map.yaml
+/usr/home/jesse/.cargo/bin/yamlink /usr/home/jesse/env -t dot -c config-map.yaml
+/usr/home/jesse/.cargo/bin/yamlink /usr/home/jesse/resource -c config-map.yaml
 
 ## ${SCRIPT_PATH}/npm_setup.sh 
 ## ${SCRIPT_PATH}/rust_setup.sh && 
