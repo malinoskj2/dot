80eff49d8c1af0672253ac8b0479654a899d389f
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-05 20:02:19 -0400

    update git-open script

--- script/sys/git-open.zsh
+++ script/sys/git-open.zsh
@@ -3,4 +3,6 @@
 # open remote giturl page
 
 BRANCH="$(git branch | grep \* | cut -d ' ' -f2)";
-$BROWSER "$(git config --get remote.origin.url)/tree/$BRANCH"
+URL="$(git config --get remote.origin.url)/tree/$BRANCH" 
+$BROWSER "$(echo $URL | sed 's/\.git//g')"
+# $BROWSER "$(git config --get remote.origin.url)/tree/$BRANCH"  
