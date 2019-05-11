86da7791f2abd2ca46cb8a57bdc2f11140c0f612
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-11 11:42:25 -0400

    add screen scaling script

---
+++ script/sys/x230_scale_1080.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# scales up res to sim 1080 on x230's lower res screen.
+# doesnt look great, 
+# sometimes u gotta do what u gotta do if u need the res.
+
+xrandr --output LVDS1 --panning 1920x1080 --scale 1.4055636896x1.40625
