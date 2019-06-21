a6ef8e4dff58e2e1cf88b5cbdb2c4cd6dfd1d39e
Jesse Malinosky <jesse@malinoskj2.dev>
2019-06-21 16:25:52 -0400

    tweaks

--- script/sys/x-reboot.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-# Logout of x and reboot system
-sudo -u jesse bspc quit && reboot
-
--- script/sys/x-shutdown.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-# Logout of x ad shutdown system
-sudo -u jesse xfce4-session-logout && shutdown
