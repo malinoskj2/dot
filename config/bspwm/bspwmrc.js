6f77b4592fdf4c43dbb9c1dba63a3042a6e557da
Jesse Malinosky <jesse@malinoskj2.dev>
2019-06-22 16:17:12 -0400

    add border to mpv

--- config/bspwm/bspwmrc.js
+++ config/bspwm/bspwmrc.js
@@ -56,7 +56,7 @@ bspwm.removeDesktop('Desktop');
 bspwm.addRule('Firefox:Navigator', 'desktop=a', 'state=tiled', 'follow=on', 'border=off', 'focus=on');
 bspwm.addRule('Alacritty', 'state=tiled', 'follow=on', 'border=on', 'focus=on');
 bspwm.addRule('IntelliJ IDEA', 'state=tiled', 'follow=on', 'border=off', 'focus=on');
-bspwm.addRule('mpv', 'state=tiled', 'desktop=b', 'follow=on', 'border=off', 'focus=on');
+bspwm.addRule('mpv', 'state=tiled', 'desktop=b', 'follow=on', 'border=on', 'focus=on');
 bspwm.addRule('polybar-mybar3_DP-2', 'focus=on', 'border=on', 'floating=on');
 bspwm.addRule('Firefox:*', 'focus=on', 'border=on', 'floating=on');
 
