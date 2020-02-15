d68fb7873e211a98057ef7b6cfefc75512c33db8
Jesse Malinosky <jesse@malinoskj2.dev>
2020-02-15 18:00:53 -0500

    revamp ss script

--- script/sys/screenshot.zsh
+++ script/sys/screenshot.zsh
@@ -1,22 +1,9 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 
 # Script to take screenshot and sometimes upload
 # ${1} = "upload" or "save" 
 # ${2} =  path to screenshot directory
 
-
-UPLOAD=""
-SS_DIR=""
-
-if [ "$1" = "upload" ]; then
-  UPLOAD="YES"
-elif [ "$1" = "save" ]; then
-  UPLOAD="NO"	
-else 
-  UPLOAD="NO" 
-fi
-
-# set SS_DIR
 if [ -z "$2" ]; then
   SS_DIR="/tmp/screenshot"
   mkdir -p "${SS_DIR}"
@@ -27,20 +14,17 @@ fi
 # create SS_DIR if it does not exist
 [ -e "$SS_DIR" ] || mkdir -p $SS_DIR 
 
-
 ## Take the SS and save
-FILE_PATH="${SS_DIR}/ss_$(date +%s).jpg"
+FILE_NAME="$(date +%s).jpg"
+FILE_PATH="${SS_DIR}/${FILE_NAME}"
 import -quality 100 "${FILE_PATH}"
 
-if [ "$UPLOAD" = "NO" ]; then
+if [ "$1" = "save" ]; then
   echo "screenshot saved @ ${FILE_PATH}" 
   notify-send -t 5000 'Capture Finished!' "${FILE_PATH}" -i ${FILE_PATH}
   echo $FILE_PATH | tr -d '\n' | xclip -selection clipboard 
 else 
-  OUTPUT=$(curl -F file="@${FILE_PATH}" \
-  https://vgy.me/upload | jq -r .url | sed -e 's/u\///g' -e 's/$/.jpg/')
-  echo $OUTPUT 
-  echo $OUTPUT | tr -d '\n' | xclip -selection clipboard &&
-  notify-send -t 5000 'Upload Finished!' "${OUTPUT}" -i ${FILE_PATH}
+  aws s3 cp --acl=public-read $FILE_PATH "s3://${SS_BUCKET}/${FILE_NAME}"
+  echo "https://${SS_BUCKET}.s3.amazonaws.com/${FILE_NAME}" | xclip -selection clipboard
 fi
 
