5f2576cc4464d606d02c93e2476a53cb7ba01eb3
Jesse Malinosky <jesse@malinoskj2.dev>
2020-05-02 05:00:24 -0400

    rename xkb config

--- config/x11/bind/map_keys.sh
+++ config/x11/bind/map_keys.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-xkbcomp -w0 -I$HOME/env/config/x11/bind/xkb2 ~/env/config/x11/bind/xkb2/keymap/mykbd $DISPLAY
+xkbcomp -w0 -I$HOME/env/config/x11/bind/xkb2 ~/env/config/x11/bind/xkb2/keymap/hhkb_j2.xkb $DISPLAY
