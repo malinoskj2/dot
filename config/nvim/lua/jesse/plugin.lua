fc3d4e0465ee8c2697f25b10095e540d25d63893
Jesse Malinosky <jesse@malinoskj2.dev>
2021-12-26 06:14:41 -0500

    automatically install packer

--- config/nvim/lua/jesse/plugin.lua
+++ config/nvim/lua/jesse/plugin.lua
@@ -1,5 +1,22 @@
 -- Plugins
 
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
 -- Install plugins
 require('packer').startup({function() 
 
@@ -51,6 +68,11 @@ use({
 	"catppuccin/nvim",
 	as = "catppuccin"
 })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 
 end,
 config = {
