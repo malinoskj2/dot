fc429164bdb7b092b111a23ab99b1c4a14a4a41f
Jesse Malinosky <jesse@malinoskj2.dev>
2021-12-26 08:32:12 -0500

    use jk to exist insert mode

--- config/nvim/lua/jesse/mapping.lua
+++ config/nvim/lua/jesse/mapping.lua
@@ -2,8 +2,11 @@
 
 local opts = { noremap = true, silent = true }
 
---Remap space as leader key
+-- Remap space as leader key
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Use jk/kj to exit Insert mode
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
+vim.api.nvim_set_keymap("i", "kj", "<ESC>", opts)
