8d456245d287ae73d72f8d14c1cabf9dac572fd6
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-03 22:11:21 -0500

    add commenting treesitter integration

--- config/nvim/lua/jesse/plugin/treesitter.lua
+++ config/nvim/lua/jesse/plugin/treesitter.lua
@@ -1,6 +1,8 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained",
+
 	sync_install = false,
+
 	autopairs = {
 		enable = true,
 	},
@@ -10,4 +12,9 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 	indent = { enable = true, disable = { "yaml" } },
+
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 })
