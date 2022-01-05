ee870a828fc9fa297fb57f3cdc84c9e77ed5e81c
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-05 18:25:07 -0500

    only Enable tailwind language server for some filetypes

--- config/nvim/lua/jesse/lsp/lsp-installer.lua
+++ config/nvim/lua/jesse/lsp/lsp-installer.lua
@@ -40,6 +40,11 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	end
 
+	if server.name == "tailwindcss" then
+		local tailwindcss_opts = require("jesse.lsp.settings.tailwindcss")
+		opts = vim.tbl_deep_extend("force", tailwindcss_opts, opts)
+	end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
