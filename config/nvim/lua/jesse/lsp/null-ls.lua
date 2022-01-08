d0160a2d023464dad8d97cb3cebbcc88346967a6
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-08 14:34:28 -0500

    stop using prettier_d_slim

--- config/nvim/lua/jesse/lsp/null-ls.lua
+++ config/nvim/lua/jesse/lsp/null-ls.lua
@@ -8,25 +8,7 @@ null_ls.setup({
 	sources = {
 		formatting.prettier.with({
 			prefer_local = "node_modules/.bin",
-			filetypes = {
-				"json",
-				"yaml",
-				"markdown",
-			},
 		}),
-		formatting.prettier_d_slim.with({
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"vue",
-				"css",
-				"scss",
-				"less",
-				"html",
-			},
-		}), -- Installed globally via npm -g, Gonna have to make a nixos derivation for this at some point
 		formatting.stylua,
 		diagnostics.eslint_d,
 	},
