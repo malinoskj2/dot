30ddb0e476c4f7392f3cdf8bd6ec804d368e8e26
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-15 15:59:16 -0500

    formatting

--- config/nvim/lua/jesse/lsp/lsp-installer.lua
+++ config/nvim/lua/jesse/lsp/lsp-installer.lua
@@ -16,15 +16,8 @@ end
 
 -- Register a handler that will be called for all installed servers.
 lsp_installer.on_server_ready(function(server)
-	local opts = {
+	server:setup(vim.tbl_deep_extend("force", {
 		on_attach = require("jesse.lsp.handlers").on_attach,
 		capabilities = require("jesse.lsp.handlers").capabilities,
-	}
-
-	-- Load server specific options if file exists in servers directory
-	opts = vim.tbl_deep_extend("force", servers[server.name] or {}, opts)
-
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
+	}, servers[server.name] or {}))
 end)
