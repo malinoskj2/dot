ec2d12c62d654ecc53f3a7c5a4dba647cc74b29a
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-15 20:50:41 -0500

    init packer in separate file

--- config/nvim/lua/jesse/plugin/init.lua
+++ config/nvim/lua/jesse/plugin/init.lua
@@ -1,35 +1,7 @@
 -- Plugins
-local fn = vim.fn
 
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
-end
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Init
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
+local packer = require("jesse.plugin.packer")
+local use = packer.use
 
 -- Install
 packer.startup(function()
