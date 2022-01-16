4010eff6596b9db4cde6c7b9d6aded5dede48f31
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-16 06:45:16 -0500

    improve telescope config

--- config/nvim/lua/jesse/plugin/config/telescope.lua
+++ config/nvim/lua/jesse/plugin/config/telescope.lua
@@ -1,7 +1,13 @@
--- Telescope
-local M = {}
+-- Telescope Configuration
+local telescope_present, telescope = pcall(require, "telescope")
+local builtin_present, builtin = pcall(require, "telescope.builtin")
 
-require("telescope").setup({
+if not telescope_present or not builtin_present then
+	return
+end
+
+-- Setup
+telescope.setup({
 	defaults = {
 		layout_config = { preview_cutoff = 160 },
 	},
@@ -15,30 +21,23 @@ require("telescope").setup({
 	},
 })
 
-require("telescope").load_extension("fzy_native")
-
--- Launch mappings
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>f",
-	"<CMD>lua require'jesse.plugin.config.telescope'.git_or_find_files()<CR>",
-	{ noremap = true, silent = true }
-)
-
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>g",
-	"<CMD>lua require('telescope.builtin').live_grep()<cr>",
-	{ noremap = true, silent = true }
-)
+-- Native FZY sorting
+telescope.load_extension("fzy_native")
 
 -- Fallback to find_files if not in git repo
-M.git_or_find_files = function()
+local git_or_find_files = function()
 	local opts = {}
-	local ok = pcall(require("telescope.builtin").git_files, opts)
+	local ok = pcall(builtin.git_files, opts)
 	if not ok then
-		require("telescope.builtin").find_files(opts)
+		builtin.find_files(opts)
 	end
 end
 
-return M
+-- Launch mappings
+vim.keymap.set("n", "<leader>f", function()
+	git_or_find_files()
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>g", function()
+	builtin.live_grep()
+end, { noremap = true, silent = true })
