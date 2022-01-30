0547c997da5e91791482b45c86fa4ff6bbde2534
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-30 16:53:22 -0500

    setup null servers for phpcs and phpcbf

--- config/nvim/lua/jesse/lsp/null_language_servers/init.lua
+++ config/nvim/lua/jesse/lsp/null_language_servers/init.lua
@@ -3,4 +3,6 @@ return {
   stylua = require "jesse.lsp.null_language_servers.stylua",
   eslint_d = require "jesse.lsp.null_language_servers.eslint_d",
   nixfmt = require "jesse.lsp.null_language_servers.nixfmt",
+  phpcbf = require "jesse.lsp.null_language_servers.phpcbf",
+  phpcs = require "jesse.lsp.null_language_servers.phpcs",
 }
