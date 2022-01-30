9b3552899a82e4af048234eca277b158ead9761d
Jesse Malinosky <jesse@malinoskj2.dev>
2022-01-30 15:47:56 -0500

    use 'vim.keymap.set' for lsp key mappings

--- config/nvim/lua/jesse/mapping/core/lsp.lua
+++ config/nvim/lua/jesse/mapping/core/lsp.lua
@@ -3,26 +3,44 @@ local List = require "pl.List"
 
 local M = {}
 
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true, buffer = 0 }
 
 local mappings = {
-  { "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>" },
-  { "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>" },
-  { "n", "gk", "<cmd>lua vim.lsp.buf.hover()<CR>" },
-  { "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>" },
-  { "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>" },
-  { "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>" },
-  { "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>" },
-  { "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>" },
-  { "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>' },
-  { "n", "gl", '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>' },
-  { "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>' },
-  { "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>" },
+  { "n", "gD", vim.lsp.buf.declaration },
+  { "n", "gd", vim.lsp.buf.definition },
+  { "n", "gk", vim.lsp.buf.hover },
+  { "n", "gi", vim.lsp.buf.implementation },
+  { "n", "<C-k>", vim.lsp.buf.signature_help },
+  { "n", "<leader>rn", vim.lsp.buf.rename },
+  { "n", "gr", vim.lsp.buf.references },
+  { "n", "<leader>ca", vim.lsp.buf.code_action },
+  {
+    "n",
+    "[d",
+    function()
+      vim.diagnostic.goto_prev { border = "rounded" }
+    end,
+  },
+  {
+    "n",
+    "gl",
+    function()
+      vim.diagnostic.open_float { border = "rounded" }
+    end,
+  },
+  {
+    "n",
+    "]d",
+    function()
+      vim.diagnostic.goto_next { border = "rounded" }
+    end,
+  },
+  { "n", "<leader>q", vim.diagnostic.setloclist },
 }
 
-M.map = function(bufnr)
+M.map = function()
   List(mappings):foreach(function(mapping)
-    vim.api.nvim_buf_set_keymap(bufnr, mapping[1], mapping[2], mapping[3], opts)
+    vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)
   end)
 end
 
