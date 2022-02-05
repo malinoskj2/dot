5711b93d8ceaa2d5b2cafcf01978009912eabed6
Jesse Malinosky <jesse@malinoskj2.dev>
2022-02-05 12:22:03 -0500

    updates

--- config/x11/bind/map_keys.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-xkbcomp -w0 -I$HOME/env/config/x11/bind/xkb2 ~/env/config/x11/bind/xkb2/keymap/hhkb_j2.xkb $DISPLAY
--- config/x11/bind/print_layout.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-setxkbmap -print -verbose 10
